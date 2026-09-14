5fe45d840b1edeeca3d3c5ebc7dc73fc651d9f89
raiyan37 <h.raiyanul@gmail.com>
2026-09-14 17:48:55 -0400

    fixed previous overexessive code. final commit for activity 4

--- utils.py
+++ utils.py
@@ -1,15 +1,14 @@
 class utils:
     def reversed(self, number):
-        """Return the integer with its digits reversed."""
-        if not isinstance(number, int):
-            raise TypeError("reversed expects an int")
+        "Return the integer with its digits reversed."
 
+        "save sign in variable"
         sign = -1 if number < 0 else 1
+        
         return sign * int(str(abs(number))[::-1])
+        "[start:stop:step], -1 steps once backwards, requires a sequence of digits hence convert to string first"
 
     def formatter(self, number):
-        """Return the number in binary (base 2) and octal (base 8) format."""
-        if not isinstance(number, int):
-            raise TypeError("formatter expects an int")
+        "Return the number in binary (base 2) and octal (base 8) format."
 
         return bin(number), oct(number)
--- utils_tests.py
+++ utils_tests.py
@@ -1,25 +1,12 @@
 from utils import utils
-
 u = utils()
 
-print("--- reversed tests ---")
-print("integer:", u.reversed(12345))
-try:
-    print("string:", u.reversed("12345"))
-except TypeError as e:
-    print("string:", e)
-try:
-    print("float:", u.reversed(12345.0))
-except TypeError as e:
-    print("float:", e)
+# reversed: string, float, integer
+print(u.reversed("12345"))
+print(u.reversed(12345.0))
+print(u.reversed(12345))
 
-print("--- formatter tests ---")
-print("integer:", u.formatter(12345))
-try:
-    print("string:", u.formatter("12345"))
-except TypeError as e:
-    print("string:", e)
-try:
-    print("float:", u.formatter(12345.0))
-except TypeError as e:
-    print("float:", e)
+# formatter: string, float, integer
+print(u.formatter("12345"))
+print(u.formatter(12345.0))
+print(u.formatter(12345))
